0929c45ed87fc1e37f916f87b791b645baa9c5d5
Eric Chapdelaine <chapdelaine.e@northeastern.edu>
2024-03-01 15:56:21 -0500

    Replace sum 2D array with async/await

--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -1,17 +1,12 @@
-function sumOfARow(arr, rowIdx) {
-    return new Promise((resolve, reject) => {
-        if (arr.length > rowIdx) {
-            setTimeout(() => {
-                let sum = 0;
-                for (let i = 0; i < arr[rowIdx].length; ++i) {
-                    sum += arr[rowIdx][i];
-                }
-                resolve(sum);
-            }, 0);
-        } else {
-            reject("invalid input")
+async function sumOfARow(arr, rowIdx) {
+    if (arr.length > rowIdx) {
+        let sum = 0;
+        for (let i = 0; i < arr[rowIdx].length; ++i) {
+            sum += arr[rowIdx][i];
         }
-    });
+        return sum;
+    }
+    throw "Index out of bounds";
 }
 
 const array2D = [
@@ -25,14 +20,18 @@ for (let x = 0; x < array2D.length; ++x) {
     rowSumPromises.push(sumOfARow(array2D, x));
 }
 
-Promise.all(rowSumPromises)
-    .then((sums) => {
-        let total = 0;
-        for (const sum of sums) {
+async function main() {
+    let total = 0;
+    try {
+
+        rowSums = await Promise.all(rowSumPromises);
+        for (const sum of rowSums) {
             total += sum;
         }
         console.log(`Total: ${total}`);
-    })
-    .catch ((err) => {
-        console.log(err);
-    });
+    } catch (err) {
+        console.err(err);
+    }
+}
+
+main();
